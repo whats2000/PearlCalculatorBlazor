8c99f62e856a07300b1465c350117302a1950826
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-06-05 01:57:58 +0800

    Delete unfinish file

--- Components/GeneralFTLComponents/GeneralFTL_General.razor.cs
+++ Components/GeneralFTLComponents/GeneralFTL_General.razor.cs
@@ -175,8 +175,6 @@ namespace PearlCalculatorBlazor.Components.GeneralFTLComponents
                 Data.BlueTNT = settings.BlueTNT;
                 Data.MaxTNT = settings.MaxTNT;
                 Data.Direction = settings.Direction;
-
-                ResultView.ShowDirectionResult(Data.Pearl.Position, Data.Destination);
             }
             catch (Exception e)
             {
