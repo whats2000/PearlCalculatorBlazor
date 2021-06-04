99eb72c6276fa13d0b43fe119d5364f04e7ac4e5
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-06-05 02:11:39 +0800

    Revert "Fix some file"
    This reverts commit 013a14d6b1aad2e014b32eb812ad8b39dd02679e.

--- Components/ResultView.razor.cs
+++ Components/ResultView.razor.cs
@@ -1,12 +1,10 @@
-﻿using AntDesign;
-using AntDesign.TableModels;
+﻿using AntDesign.TableModels;
 using PearlCalculatorBlazor.Managers;
 using PearlCalculatorLib.General;
-using PearlCalculatorLib.PearlCalculationLib.Entity;
+using PearlCalculatorLib.PearlCalculationLib.World;
 using PearlCalculatorLib.Result;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -34,7 +32,64 @@ namespace PearlCalculatorBlazor.Components
         private List<EntityWrapper> PearlTrace { get; set; } = new();
         private List<EntityWrapper> PearlMotion { get; set; } = new();
 
+        private static string ResultDirection = string.Empty;
 
+        private static string ResultAngle = string.Empty;
+
+        public static void ShowDirectionResult(Space3D pearlPos, Space3D destination)
+        {
+            var angle = pearlPos.WorldAngle(destination);
+            if (angle == 370)
+                return;
+            ResultDirection = pearlPos.Direction(angle).ToString();
+            ResultAngle = angle.ToString();
+        }
+
+        private static double AccurateWorldAngle(Space3D position1, Space3D position2)
+        {
+            Space3D distance = new Space3D();
+            distance.X = position2.X - position1.X;
+            distance.Z = position2.Z - position1.Z;
+            double phi;
+            if (distance.X == 0 && distance.Z == 0)
+                return 370;
+            phi = Math.Atan(Math.Abs(distance.X / distance.Z));
+            if (distance.X > 0)
+            {
+                if (distance.Z > 0)
+                    return -(Math.Atan(distance.X / distance.Z) * 180 / Math.PI);
+                else if (distance.Z == 0)
+                    return -90;
+                else
+                    return -(180 - Math.Atan(-distance.X / distance.Z) * 180 / Math.PI);
+            }
+            else if (distance.X < 0)
+            {
+                if (distance.Z > 0)
+                    return Math.Atan(-distance.X / distance.Z) * 180 / Math.PI;
+                else if (distance.Z == 0)
+                    return 90;
+                else
+                    return 180 - Math.Atan(distance.X / distance.Z) * 180 / Math.PI;
+            }
+            else
+            {
+                if (distance.Z > 0)
+                    return 0;
+                else
+                    return 180;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private static void AccurateDirectionResult(Space3D pearlPos, Space3D destination)
+        {
+            var angle = AccurateWorldAngle(pearlPos, destination);
+            if (angle == 370)
+                return;
+            ResultDirection = pearlPos.Direction(angle).ToString();
+            ResultAngle = angle.ToString();
+        }
 
         protected override void OnInitialized()
         {
@@ -45,6 +100,9 @@ namespace PearlCalculatorBlazor.Components
                 AmountResult = Data.TNTResult;
 
                 ShowMode = ShowResultMode.Amount;
+
+                ShowDirectionResult(Data.Pearl.Position, Data.Destination);
+
                 StateHasChanged();
             });
 
@@ -64,7 +122,10 @@ namespace PearlCalculatorBlazor.Components
                     };
                 }).ToList());
 
+                AccurateDirectionResult(Data.Pearl.Position, new Space3D(PearlTrace[1].XCoor, PearlTrace[1].YCoor, PearlTrace[1].ZCoor));
+                
                 ShowMode = ShowResultMode.Trace;
+                
                 StateHasChanged();
             });
 
@@ -73,6 +134,9 @@ namespace PearlCalculatorBlazor.Components
                 if (ShowMode != ShowResultMode.Amount || AmountResult is null) return;
 
                 AmountResult.SortByWeightedDistance(new(Data.TNTWeight, Data.MaxCalculateTNT, Data.MaxCalculateDistance));
+
+                ShowDirectionResult(PearlCalculatorLib.Manually.Data.Pearl.Position, PearlCalculatorLib.Manually.Data.Destination.ToSpace3D());
+                
                 StateHasChanged();
             });
 
@@ -85,6 +149,7 @@ namespace PearlCalculatorBlazor.Components
                 if (ShowMode != ShowResultMode.Amount || AmountResult is null) return;
 
                 AmountResult.SortByWeightedDistance(new(Data.TNTWeight, Data.MaxCalculateTNT, Data.MaxCalculateDistance));
+
                 StateHasChanged();
             });
 
@@ -105,6 +170,9 @@ namespace PearlCalculatorBlazor.Components
                 }).ToList());
 
                 ShowMode = ShowResultMode.Trace;
+
+                AccurateDirectionResult(PearlCalculatorLib.Manually.Data.Pearl.Position, new Space3D(PearlTrace[1].XCoor, PearlTrace[1].YCoor, PearlTrace[1].ZCoor));
+
                 StateHasChanged();
             });
 
@@ -125,6 +193,9 @@ namespace PearlCalculatorBlazor.Components
                 }).ToList());
 
                 ShowMode = ShowResultMode.Momentum;
+
+                AccurateDirectionResult(PearlCalculatorLib.Manually.Data.Pearl.Position, new Space3D(PearlTrace[1].XCoor, PearlTrace[1].YCoor, PearlTrace[1].ZCoor));
+
                 StateHasChanged();
             });
         }
@@ -133,7 +204,6 @@ namespace PearlCalculatorBlazor.Components
         {
             EventManager.Instance.PublishEvent(this, "tntAmountSetRTCount", new SetRTCountArgs("ResultView", res.Data.Red, res.Data.Blue));
         }
-
     }
 
     public class EntityWrapper
