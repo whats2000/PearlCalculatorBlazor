96e32c0c1a14f9fd3477c4b5cf0ff290619a2fd0
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-06-14 00:31:42 +0800

    i18n : add select translate to mobile

--- Components/GeneralFTL.razor.cs
+++ Components/GeneralFTL.razor.cs
@@ -1,16 +1,35 @@
 ﻿using PearlCalculatorBlazor.Localizer;
+using System.Collections.Generic;
 
 namespace PearlCalculatorBlazor.Components
 {
     public partial class GeneralFTL
     {
+        class Array
+        {
+            public string Key { get; set; }
+            public string DisplayName { get; set; }
+        }
+        
+        private List<Array> _selectList;
+
         protected override void OnInitialized()
         {
+            _selectList = new List<Array>
+            {
+                new Array {Key = "GeneralFTLGeneralHeader", DisplayName = TranslateText.GetTranslateText("GeneralFTLGeneralHeader")},
+                new Array {Key = "GeneralFTLAdvancedHeader", DisplayName = TranslateText.GetTranslateText("GeneralFTLAdvancedHeader")},
+                new Array {Key = "GeneralFTLSettingsHeader", DisplayName = TranslateText.GetTranslateText("GeneralFTLSettingsHeader")}
+            };
+
             TranslateText.OnLanguageChange += RefreshPage;
         }
 
         public void RefreshPage()
         {
+            foreach (var pair in _selectList)
+                pair.DisplayName = TranslateText.GetTranslateText(pair.Key);
+
             StateHasChanged();
         }
     }
