57b5bb3ca43c30940c0eac96162e6766fdfbe14d
whats2000 <60466660+whats2000@users.noreply.github.com>
2024-08-22 03:00:18 +0800

    Add: Add language setting cache

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,17 +1,34 @@
-﻿using PearlCalculatorBlazor.Localizer;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PearlCalculatorBlazor.Localizer;
 
 namespace PearlCalculatorBlazor.Pages
 {
     public partial class Index
     {
+        [Inject] IJSRuntime JSRuntime { get; set; }
+
         private async void OnClickChangeLanguage(string language)
         {
+            // Load the selected language
             await TransText.LoadLanguageAsync(language);
+
+            // Store the selected language in localStorage
+            await JSRuntime.InvokeVoidAsync("localStorage.setItem", "userLanguage", language);
         }
 
-        protected override void OnInitialized()
+        protected override async void OnInitialized()
         {
             TranslateText.OnLanguageChange += RefreshPage;
+
+            // Check if a language is stored in localStorage
+            var storedLanguage = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "userLanguage");
+
+            if (!string.IsNullOrEmpty(storedLanguage))
+            {
+                // Load the stored language
+                await TransText.LoadLanguageAsync(storedLanguage);
+            }
         }
 
         public void RefreshPage()
