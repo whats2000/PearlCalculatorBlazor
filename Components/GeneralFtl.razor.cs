02166fbef27b024e71458a6a53bd4ddcc4409b6a
whats2000 <whats2000mc@gmail.com>
2024-09-04 12:48:44 +0800

    Feat: Optimize the name of the component

---
+++ Components/GeneralFtl.razor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using PearlCalculatorBlazor.Localizer;
+
+namespace PearlCalculatorBlazor.Components;
+
+public partial class GeneralFtl
+{
+    private List<Array> _selectList;
+
+    protected override void OnInitialized()
+    {
+        _selectList = new List<Array>
+        {
+            new()
+            {
+                ActiveKey = "GeneralFtlGeneral", DisplayName = TranslateText.GetTranslateText("GeneralFtlGeneral")
+            },
+            new()
+            {
+                ActiveKey = "GeneralFtlAdvanced", DisplayName = TranslateText.GetTranslateText("GeneralFtlAdvanced")
+            },
+            new()
+            {
+                ActiveKey = "GeneralFtlSettings", DisplayName = TranslateText.GetTranslateText("GeneralFtlSettings")
+            }
+        };
+
+        TranslateText.OnLanguageChange += RefreshPage;
+    }
+
+    private void RefreshPage()
+    {
+        foreach (var pair in _selectList)
+            pair.DisplayName = TranslateText.GetTranslateText(pair.ActiveKey);
+
+        StateHasChanged();
+    }
+
+    private class Array
+    {
+        public string DisplayName { get; set; }
+        public string ActiveKey { get; set; }
+    }
+}
---
+++ Components/GeneralFtlComponents/GeneralFtlAdvanced.razor.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.JSInterop;
+using PearlCalculatorBlazor.Localizer;
+using PearlCalculatorBlazor.Managers;
+using PearlCalculatorLib.General;
+using PearlCalculatorLib.PearlCalculationLib.World;
+
+namespace PearlCalculatorBlazor.Components.GeneralFtlComponents;
+
+public enum SortMode
+{
+    SortByWeightedDistance,
+    SortByWeightedTotal
+}
+
+public partial class GeneralFtlAdvanced
+{
+    private static GeneralFtlAdvanced _instance;
+    private static SortMode _sortBy = SortMode.SortByWeightedDistance;
+
+    public GeneralFtlAdvanced()
+    {
+        _instance = this;
+    }
+
+    private double PearlOffsetX
+    {
+        get => Data.PearlOffset.X;
+        set => Data.PearlOffset = new Surface2D(value, PearlOffsetZ);
+    }
+
+    private double PearlOffsetZ
+    {
+        get => Data.PearlOffset.Z;
+        set => Data.PearlOffset = new Surface2D(PearlOffsetX, value);
+    }
+
+    private double TntWeight
+    {
+        get => Data.TNTWeight;
+        set => Data.TNTWeight = (int)value;
+    }
+
+    private SortMode SelectSortMode
+    {
+        get => _sortBy;
+        set
+        {
+            _sortBy = value;
+            StateHasChanged();
+        }
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (!firstRender) return;
+        ((IJSInProcessRuntime)JsRuntime).InvokeVoid("AddTNTWeightSliderEvent");
+    }
+
+    private void ChangeTntWeight()
+    {
+        var isSortByWeightedDistance = SelectSortMode == SortMode.SortByWeightedDistance;
+        EventManager.Instance.PublishEvent(this,
+            isSortByWeightedDistance ? "sortByWeightedDistance" : "sortByWeightedTotal",
+            new ButtonClickArgs("GeneralFtlAdvanced"));
+    }
+
+    [JSInvokable]
+    public static void ChangeTntWeightJs()
+    {
+        _instance.ChangeTntWeight();
+    }
+
+    protected override void OnInitialized()
+    {
+        TranslateText.OnLanguageChange += RefreshPage;
+    }
+
+    private void RefreshPage()
+    {
+        StateHasChanged();
+    }
+}
