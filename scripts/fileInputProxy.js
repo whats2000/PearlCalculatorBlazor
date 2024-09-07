08ed47e63c2aa4929738bbf26033f296472aa305
whats2000 <whats2000@users.noreply.github.com>
2024-09-07 20:51:31 +0000

    Deploy to GitHub pages

---
+++ scripts/fileInputProxy.js
@@ -0,0 +1,26 @@
+﻿var importSettingsHandle;
+var downloadSettingsLink;
+
+function ImportSettings() {
+
+    if (!importSettingsHandle) {
+        importSettingsHandle = document.getElementById("import-settings-handle");
+    }
+    return importSettingsHandle.click();
+}
+
+function ExportSettings(json) {
+    if (!downloadSettingsLink) {
+        downloadSettingsLink = document.getElementById("download-settings-link");
+    }
+
+    var blob = new Blob([json]);
+    downloadSettingsLink.href = URL.createObjectURL(blob);
+    downloadSettingsLink.download = "pearl calculator settings.json";
+    downloadSettingsLink.click();
+}
+
+function ResetStateInJs() {
+    importSettingsHandle = null;
+    downloadSettingsLink = null;
+}
