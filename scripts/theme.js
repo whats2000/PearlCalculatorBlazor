697c5c9e855d2f7e4aa22b8e7be6bfbb84ce1bc4
whats2000 <whats2000@users.noreply.github.com>
2025-03-08 10:45:35 +0000

    Deploy to GitHub pages

---
+++ scripts/theme.js
@@ -0,0 +1,14 @@
+﻿function AddElementToBody(url) {
+    let link = document.getElementById('dynamic-theme');
+    if (!link) {
+        link = document.createElement('link');
+        link.id = 'dynamic-theme';
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+    }
+    link.href = url;
+}
+
+function GetBaseUrl() {
+    return window.location.origin + window.location.pathname;
+}
---
+++ scripts/tntWeightSlider.js
@@ -0,0 +1,15 @@
+﻿var isTntWeightSliderMouseDown = false;
+
+function AddTNTWeightSliderEvent() {
+
+    document.getElementById("tnt-weight-slider").onmousedown = () => {
+        isTntWeightSliderMouseDown = true;
+    }
+
+    document.onmouseup = () => {
+        if (isTntWeightSliderMouseDown) {
+            isTntWeightSliderMouseDown = false;
+            DotNet.invokeMethod("PearlCalculatorBlazor", "ChangeTntWeightJs");
+        }
+    }
+}
